c38bf7da8d98fa08f493f963d075f569d02ce968
Mattbarbo97 <138821344+Mattbarbo97@users.noreply.github.com>
2024-09-16 22:17:21 -0300

    Add files via upload

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './Footer.css';
 
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Victor Mendes - Todos os direitos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
+function Header() {
+  return (
+    <header className="header">
+      <h1>Victor Mendes</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+          <li><Link to="/contato">Contato</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
